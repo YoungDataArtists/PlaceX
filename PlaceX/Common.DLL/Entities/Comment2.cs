9fe146e148de3f0247d76dec3c929be7f5d9366a
tanya-112 <tanya.kumar11.02@gmail.com>
2017-10-19 12:48:53 +0300

    test2

---
+++ PlaceX/Common.DLL/Entities/Comment2.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DLL.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime DateOfComment { get; set; }
+        public User CommentAuthor { get; set; }
+        public Visibility VisibleFor { get; set; }
+
+        public virtual ICollection<Like> CommentLikes { get; set; }
+    }
+}
