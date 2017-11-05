5fdc80ef22357a5c95baab40fadb2676ed6c1a28
Dima <dmyhun@gmail.com>
2017-11-05 15:42:32 +0200

    US-12-BE and US-13-BE
    Create PlaceViewModel. Create a pop-up window by left mouse clicking on the place icon on the map. Create Bacis Place Page.

--- PlaceX/PlaceX/Models/PlaceViewModel.cs
+++ PlaceX/PlaceX/Models/PlaceViewModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace PlaceX.Models
+﻿namespace PlaceX.Models
 {
     public class PlaceViewModel
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IconPath { get; set; }
+        public string GoogleRating { get; set; }
     }
 }
