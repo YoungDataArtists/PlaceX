a14daa5672eb21f9e6513b6d559642c22b927123
AndreyParhomchuk <32749654+AndreyParhomchuk@users.noreply.github.com>
2017-11-16 23:45:31 +0200

    PlaceInfo some changes
    - added unstable slider
    - comments template

--- PlaceX/PlaceX/Scripts/Controllers/HomeController.js
+++ PlaceX/PlaceX/Scripts/Controllers/HomeController.js
@@ -190,4 +190,22 @@
             "width": "100%"
         });
     }
+    //slider for placeinfo 
+    $('.carousel[data-type="multi"] .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 2; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+
     
