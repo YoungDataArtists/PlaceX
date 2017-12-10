244f14afb612277eab4725fa7715221d035e6c7d
Татьяна <tanya.kumar11.02@gmail.com>
2017-12-10 13:42:36 +0200

    Allow user to log out

--- PlaceX/PlaceX/Controllers/HomeController.cs
+++ PlaceX/PlaceX/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace PlaceX.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         public ActionResult PlaceInfo(string placeId)
         {
             string placeInfo = GetPlaceJson("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=AIzaSyArQis35LbcmrOvvVlAJtsHABL7ObLubk8&language=ru");
